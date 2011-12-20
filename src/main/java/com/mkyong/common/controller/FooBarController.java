59f48f1b43f2a1fd644280303192fd27bcd2cebf
Brendon Van Heyzen <brendon2020@mac.com>
2011-12-19 21:13:17 -0500

    first commit

---
+++ src/main/java/com/mkyong/common/controller/FooBarController.java
@@ -0,0 +1,20 @@
+package com.mkyong.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/bar")
+public class FooBarController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+        model.addAttribute("message","foobar!!!");
+
+        return "hello";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+   <h1> asdfasdf Message : ${message}</h1>
+</body>
+</html>
